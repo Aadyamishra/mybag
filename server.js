b714cb7e711690f7740eddcbaff98e9b042b3946
Aadyamishra <palakm1107@gmail.com>
2017-03-09 15:06:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ function drawName(){
 drawName(myName, letterColors);}
 function bounceBubbles(){
 bounceBubbles();}
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join( 'index.html'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`listening on port ${port}!`);
